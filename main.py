f437da23b3d79eea23b1cde41adbe32121c452ed
qmzsky <775832781@qq.com>
2022-04-21 20:30:40 +0800

    3.3

--- main.py
+++ main.py
@@ -282,12 +282,22 @@ def main(args):
             prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
             label_smoothing=args.smoothing, num_classes=args.nb_classes)
 
-    model = create_ConvNextPlus(
-        pretrained=False,
-        num_classes=args.nb_classes, 
-        drop_path_rate=args.drop_path,
-        layer_scale_init_value=args.layer_scale_init_value,
-        head_init_scale=args.head_init_scale,
+    if args.model == "convNextPlus":
+        model = create_ConvNextPlus(
+            pretrained=False,
+            num_classes=args.nb_classes,
+            drop_path_rate=args.drop_path,
+            layer_scale_init_value=args.layer_scale_init_value,
+            head_init_scale=args.head_init_scale,
+            )
+    else:
+        model = create_model(
+            args.model,
+            pretrained=False,
+            num_classes=args.nb_classes,
+            drop_path_rate=args.drop_path,
+            layer_scale_init_value=args.layer_scale_init_value,
+            head_init_scale=args.head_init_scale,
         )
 
     if args.finetune:
